2d13496f98899b3a20e46f6479ecca50737755bc
Manvendra Singh <mjdeora@gmail.com>
2022-05-19 14:43:57 +0530

    Option page bind to icon click

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,2 +1,6 @@
 console.log('This is the background page.');
 console.log('Put the background scripts here.');
+
+chrome.action.onClicked.addListener(() => {
+  chrome.runtime.openOptionsPage();
+});
